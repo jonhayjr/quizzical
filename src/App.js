0dc870111cd39c1b7b2a3dbc2a0d8a3e990e7d5a
Jon Hay <33227538+jonhayjr@users.noreply.github.com>
2022-04-11 18:51:24 -0700

    Created start game overlay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './style.css';
 
+/*Import Components*/  
+import StartOverlay from './components/StartOverlay';
+
 const App = () => {
   return (
     <div className="container">
-      
+      <StartOverlay/>
     </div>
   );
 }
---
+++ src/components/StartOverlay.js
@@ -0,0 +1,11 @@
+const StartOverlay = () => {
+    return (
+        <div className="overlay">
+            <h1 className="title">Quizzical</h1>
+            <p className="subtitle">Some description if needed</p>
+            <button className="btn btn__start">Start quiz</button>
+        </div>
+    )
+}
+
+export default StartOverlay;
