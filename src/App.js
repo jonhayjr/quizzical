e15703c723f8621ff62165bfd1374d0765f88dbf
Jon Hay <33227538+jonhayjr@users.noreply.github.com>
2022-04-11 18:55:50 -0700

    Added start to handle start game toggle.  Added hover and focus effects to button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, {useState} from 'react';
 import './style.css';
 
 /*Import Components*/  
 import StartOverlay from './components/StartOverlay';
 
 const App = () => {
+  const [isStarted, setIsStarted] = useState(false);
+
+  const toggleStartGame = () => {
+    setIsStarted(prev => !prev);
+  }
   return (
     <div className="container">
-      <StartOverlay/>
+      {!isStarted && <StartOverlay toggleStartGame={toggleStartGame}/>}
     </div>
   );
 }
--- src/components/StartOverlay.js
+++ src/components/StartOverlay.js
@@ -1,9 +1,9 @@
-const StartOverlay = () => {
+const StartOverlay = (props) => {
     return (
         <div className="overlay">
             <h1 className="title">Quizzical</h1>
             <p className="subtitle">Some description if needed</p>
-            <button className="btn btn__start">Start quiz</button>
+            <button className="btn btn__start" onClick={props.toggleStartGame}>Start quiz</button>
         </div>
     )
 }
